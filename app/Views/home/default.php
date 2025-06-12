7b719b2117677b7fbe13ff026587e26b4a23f330
Edicleiton Alves <edicleiton16@gmail.com>
2025-06-12 20:36:32 -0300

    Subida pixel meta

--- app/Views/home/default.php
+++ app/Views/home/default.php
@@ -29,7 +29,7 @@ define('CONFIGURACAO', [
 	<meta name="theme-color" content="#075600">
 	<meta name="apple-mobile-web-app-status-bar-style" content="#075600">
 	<meta name="msapplication-navbutton-color" content="#075600">
-	<meta name="facebook-domain-verification" content="3dhv8twukg8lor5ypbedcr8ypaweaq" />
+	<meta name="facebook-domain-verification" content="idwmp814fyc9txijorxzr4l3czxt5a" />
 	<link rel="icon" href="/img/favicon.png" type="image/x-icon">
 	<title>Início | Agility Telecom</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -61,6 +61,30 @@ define('CONFIGURACAO', [
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAip-QesZTgTNaIZ21zKK4cv9GspTQA6p0" defer></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js"></script>
 	<script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+	<script>
+		! function(f, b, e, v, n, t, s) {
+			if (f.fbq) return;
+			n = f.fbq = function() {
+				n.callMethod ?
+					n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+			};
+			if (!f._fbq) f._fbq = n;
+			n.push = n;
+			n.loaded = !0;
+			n.version = '2.0';
+			n.queue = [];
+			t = b.createElement(e);
+			t.async = !0;
+			t.src = v;
+			s = b.getElementsByTagName(e)[0];
+			s.parentNode.insertBefore(t, s)
+		}(window, document, 'script',
+			'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '1349392152818432');
+		fbq('track', 'PageView');
+	</script>
+	<noscript><img height="1" width="1" style="display:none"
+			src="https://www.facebook.com/tr?id=1349392152818432&ev=PageView&noscript=1" /></noscript>
 	<script>
 		new window.VLibras.Widget('https://vlibras.gov.br/app');
 	</script>
